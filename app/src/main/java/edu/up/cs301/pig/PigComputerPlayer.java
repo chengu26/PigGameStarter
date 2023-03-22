b981d98b4a10a5cdd73d74d9237ef44a18d0cb76
starrnakamitsu <nakamits26@up.edu>
2023-03-21 18:38:57 -0700

    check

--- app/src/main/java/edu/up/cs301/pig/PigComputerPlayer.java
+++ app/src/main/java/edu/up/cs301/pig/PigComputerPlayer.java
@@ -28,7 +28,8 @@ public class PigComputerPlayer extends GameComputerPlayer {
      */
     @Override
     protected void receiveInfo(GameInfo info) {
-        // TODO  You will implement this method
+        PigGameState pigGameState = new PigGameState((PigGameState) info);
+        if (pigGameState.getPlayerID() == )
     }//receiveInfo
 
 }
